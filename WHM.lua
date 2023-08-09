2fe6eb41855ccad827967d85cd145b88ffe8c468
Renzoth <ryanboocock26@gmail.com>
2023-08-09 07:30:05 +0100

    Create WHM.lua

---
+++ WHM.lua
@@ -0,0 +1,100 @@
+local profile = {};
+local sets = {
+    ['Idle_Priority'] = {
+        Main = {'Earth Staff'},
+        -- Sub = {'Maple Shield'},
+        Head = {'Bastokan Circlet'},
+        Neck = {'Justice Badge'},
+        Ear1 = {'Energy Earring'},
+        Ear2 = {'Energy Earring'},
+        Body = {'Seer\'s Tunic'},
+        Hands = {'Savage Gauntlets'},
+        Ring1 = {'Tamas Ring'},
+        Ring2 = {'Astral Ring'},
+        Back = {'White Cape +1'},
+        Waist = {'Friar\'s Rope'},
+        Legs = {'Savage Loincloth'},
+        Feet = {'Seer\'s Pumps +1'},
+    },
+};
+profile.Sets = sets;
+
+profile.Packer = {
+};
+
+local Settings = {
+    currentLevel = 0;
+}
+
+profile.OnLoad = function()
+    gSettings.AllowAddSet = true;
+end
+
+profile.OnUnload = function()
+end
+
+profile.HandleCommand = function(args)
+end
+
+profile.HandleDefault = function()
+    local player = gData.GetPlayer();
+    local myLevel = AshitaCore:GetMemoryManager():GetPlayer():GetMainJobLevel();
+
+    if (myLevel ~= Settings.CurrentLevel) then
+        gFunc.EvaluateLevels(profile.Sets, myLevel);
+        Settings.CurrentLevel = myLevel;
+    end
+
+    if (player.Status == 'Resting') then
+        if (player.SubJob == 'BLM') then
+            gFunc.Equip('Back', 'Wizard\'s Mantle')
+            if (player.MainJobSync < 51) then
+                gFunc.Equip('Main', 'Blessed Hammer');
+            else
+                gFunc.Equip('Main', 'Dark Staff');
+            end
+        end
+        
+        gFunc.Equip('Neck', 'Checkered Scarf');
+        if (player.MainJobSync < 59) then
+            gFunc.Equip('Body', 'Seer\'s Tunic');
+        end
+        gFunc.Equip('Legs', 'Baron\'s Slops');
+    else
+        gFunc.EquipSet('Idle');
+    end
+end
+
+profile.HandleAbility = function()
+end
+
+profile.HandleItem = function()
+end
+
+profile.HandlePrecast = function()
+end
+
+profile.HandleMidcast = function()
+    local action = gData.GetAction();
+
+    if (string.match(action.Name, 'Cure')) then
+        gFunc.Equip('Main', 'Light Staff');
+        -- gFunc.EquipSet('MND');
+    end
+
+    if (string.match(action.Name, 'Banish')) or (action.Name == 'Holy') then
+        gFunc.Equip('Main', 'Light Staff');
+        gFunc.Equip('Ear1', 'Moldavite Earring');
+    end
+end
+
+profile.HandlePreshot = function()
+end
+
+profile.HandleMidshot = function()
+end
+
+profile.HandleWeaponskill = function()
+end
+
+return profile;
