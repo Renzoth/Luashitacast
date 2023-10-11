9a4baa63b1d02fa2e53494cb9358c43c986ce957
Renzoth <ryanboocock26@gmail.com>
2023-10-11 18:12:38 +0100

    Create DRG.lua

---
+++ DRG.lua
@@ -0,0 +1,48 @@
+local profile = {};
+local sets = {
+};
+profile.Sets = sets;
+
+profile.Packer = {
+};
+
+profile.OnLoad = function()
+    gSettings.AllowAddSet = true;
+end
+
+profile.OnUnload = function()
+end
+
+profile.HandleCommand = function(args)
+end
+
+profile.HandleDefault = function()
+    local player = gData.GetPlayer();
+
+    if (player.HPP <= 33) then
+        gFunc.Message("HEAL!")
+    end
+end
+
+profile.HandleAbility = function()
+end
+
+profile.HandleItem = function()
+end
+
+profile.HandlePrecast = function()
+end
+
+profile.HandleMidcast = function()
+end
+
+profile.HandlePreshot = function()
+end
+
+profile.HandleMidshot = function()
+end
+
+profile.HandleWeaponskill = function()
+end
+
+return profile;
